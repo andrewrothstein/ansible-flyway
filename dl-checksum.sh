bc57895ba18864419036c5e87fe3b96522b305ac
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-11-03 15:50:10 -0500

    up to v6.0.8

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env sh
-VER=${1:-6.0.7}
 DIR=~/Downloads
-FILE=flyway-commandline-${VER}.tar.gz
-LFILE=$DIR/$FILE
-URL=https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/${VER}/$FILE
 
-if [ ! -e $LFILE ];
-then
-    wget -q -O $LFILE $URL
-fi
+dl_ver() {
+    local ver=$1
+    local file=flyway-commandline-${ver}.tar.gz
+    local lfile=$DIR/$file
+    local url=https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/${ver}/$file
 
-printf "  # %s\n" $URL
-printf "  '%s': sha256:%s\n" $VER $(sha256sum $LFILE | awk '{print $1}')
+    if [ ! -e $lfile ];
+    then
+        wget -q -O $lfile $url
+    fi
+
+    printf "  # %s\n" $url
+    printf "  '%s': sha256:%s\n" $ver $(sha256sum $lfile | awk '{print $1}')
+}
+
+dl_ver ${1:-6.0.8}
